2ec6d1bc2c54a533a3a62169f542ac9ffbaec097
Joshi-hub <joshuaauerbach@web.de>
2026-05-27 10:59:50 +0200

    Add user registration API endpoint and initial database migrations

--- quizly_backend/urls.py
+++ quizly_backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
-    path('api/', include('quizzes.urls')),
+    path('api/', include('users.api.urls')),
+    # path('api/', include('quizzes.api.urls')),
 ]
---
+++ users/api/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    confirmed_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirmed_password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data['confirmed_password']:
+            raise serializers.ValidationError({'confirmed_password': 'Passwords do not match.'})
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirmed_password')
+        return User.objects.create_user(**validated_data)
